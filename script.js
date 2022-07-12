821140b9be00a71d338f5f81b7779f1d39832202
orkatayev <orbadi@gmail.com>
2022-07-13 00:35:24 +0300

    git

---
+++ script.js
@@ -0,0 +1,7 @@
+$(".images img").click(function(){
+    $("#full-image").attr("src", $(this).attr("src"));
+    $('#image-viewer').show();
+  });
+  $("#image-viewer .close").click(function(){
+    $('#image-viewer').hide();
+  });
